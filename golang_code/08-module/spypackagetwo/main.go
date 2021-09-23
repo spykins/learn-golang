81cb57d9ac7e02dead7a08dafbbe852d4882ce84
Waleola Akinsanmi <waleola.akinsanmi@gmail.com>
2021-09-23 16:35:37 -0600

    run go program in the same module and in another module

--- golang_code/08-module/spypackagetwo/main.go
+++ golang_code/08-module/spypackagetwo/main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 )
 
+// run with >> GO111MODULE=off go run .
+// # this search module in the path locally,
+// # when GO111MODULE=on it pulls it from internet, GO111MODULE=auto...
 func main() {
 	fmt.Println("This is the main function")
 	spypackageone.SayHello()
